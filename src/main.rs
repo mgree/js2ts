69883a310a2952cd45c2be41d02f664d491aafe9
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2023-01-30 14:13:08 -0500

    Added basic cli

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,29 @@ use swc_common::sync::Lrc;
 use swc_ecma_parser::{StringInput, Syntax, Parser};
 use swc_ecma_parser::lexer::Lexer;
 
+use clap::Parser as ClapParser;
+
+#[derive(ClapParser)]
+struct Args {
+    filename: String,
+}
+
 fn main() {
+    let args = Args::parse();
+    let file = match std::fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("could not open file `{}`: {}", args.filename, e);
+            std::process::exit(1);
+        }
+    };
+
     let cm = Lrc::<SourceMap>::default();
-    let handler = Handler::with_tty_emitter(ColorConfig::Always, true, true, Some(cm.clone()));
+    let handler = Handler::with_tty_emitter(ColorConfig::Always, true, false, Some(cm.clone()));
 
     let fm = cm.new_source_file(
-        FileName::Custom("test.js".into()),
-        "function foo() {}".into()
+        FileName::Custom(args.filename.into()),
+        file.into()
     );
 
    let lexer = Lexer::new(
@@ -28,11 +44,11 @@ fn main() {
         e.into_diagnostic(&handler).emit();
     }
 
-    let _module = parser
-        .parse_module()
-        .map_err(|e| {
-            // Unrecoverable fatal error occurred
-            e.into_diagnostic(&handler).emit()
-        })
-        .expect("failed to parser module");
+    let _module = match parser.parse_module() {
+        Ok(v) => v,
+        Err(e) => {
+            e.into_diagnostic(&handler).emit();
+            std::process::exit(1);
+        }
+    };
 }
